24cf34c6a9b190727b9aa6e6539c22e964319ed7
ahmetaa <ahmetaa@gmail.com>
2018-08-13 23:05:28 +0300

    do not use resources for libraries for now.

--- src/kaldijni/KaldiAcousticScorer.java
+++ src/kaldijni/KaldiAcousticScorer.java
@@ -1,27 +1,20 @@
 package kaldijni;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class KaldiAcousticScorer {
 
-
     static {
-        try {
-            String osName = System.getProperty("os.name");
-            if (osName.contains("Windows")) {
-                throw new IllegalStateException("Windows is not supported yet");
-            } else if (osName.contains("Linux")) {
-                NativeUtils.loadLibraryFromJar("/resources/kaldi-jni.so");
-            } else {
-                throw new IllegalStateException("There is no library for OS = " + osName);
-            }
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        String osName = System.getProperty("os.name");
+        if (osName.contains("Windows")) {
+            throw new IllegalStateException("Windows is not supported yet");
+        } else if (osName.contains("Linux")) {
+            System.loadLibrary("kaldi-jni");
+        } else {
+            throw new IllegalStateException("There is no library for OS = " + osName);
         }
     }
-
 
     private long nativeHandle;
 
